191c1c2f470f47fdf168d3cffc5c093b04c4bbc4
am.soheili <am.soheili@mydigipay.com>
2025-10-23 11:57:42 +0330

    Diamond created

--- diamond-maker.go
+++ diamond-maker.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello")
+	maxWidth := 101
+	fmt.Println(" ") // 50 49 48
+	// stars 1 - 3 - 5 - .... - 101 - 99 - 97 - ....  - 1
+	var isUpperTriangle bool = true
+
+	spaceNo := (maxWidth - 1) / 2
+
+	starNo := 1
+
+	for i := 0; i < maxWidth; i++ {
+		for spi := 0; spi < spaceNo; spi++ {
+			fmt.Print(" ")
+		}
+		for sti := 0; sti < starNo; sti++ {
+			fmt.Print("*")
+		}
+		if starNo == maxWidth {
+			isUpperTriangle = false
+		}
+		if isUpperTriangle {
+			spaceNo--
+			starNo += 2
+		} else {
+			spaceNo++
+			starNo -= 2
+		}
+		fmt.Println()
+	}
 }
